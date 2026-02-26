486b823433b7801423f7b010a2d950525b1af80f
Half-0001 <108597230+Half-0001@users.noreply.github.com>
2026-02-26 08:45:00 +0000

    implmementation almost done

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -18,6 +18,9 @@ repositories {
     mavenCentral()
 }
 
+val exposedVersion = "1.1.0"
+
+
 dependencies {
     // Use the Kotlin Test integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -35,6 +38,18 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+
+    //database 
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.h2)
+    
+    // exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
---
+++ code/app/src/main/kotlin/org/flightbooking/database/Access.kt
@@ -0,0 +1,36 @@
+package access
+
+import models.Flights
+import tables.FlightsTable
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.select
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.jetbrains.exposed.v1.core.like
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.core.ResultRow
+
+class FlightAccess {
+    
+    fun getAll(): List<Flights> = transaction {
+        FlightsTable.selectAll().map {
+            constructFlight(it)
+        }
+    }
+
+
+    fun constructFlight(it: ResultRow): Flights {
+        return Flights (
+            id = it[FlightsTable.id],
+            flightNumber = it[FlightsTable.flightNumber],
+            departureAirport = it[FlightsTable.departureAirport],
+            arrivalAirport = it[FlightsTable.arrivalAirport],
+            departureTime = it[FlightsTable.departureTime],
+            arrivalTime = it[FlightsTable.arrivalTime],
+            price = it[FlightsTable.price],
+            totalSeats = it[FlightsTable.totalSeats],
+            availableSeats = it[FlightsTable.availableSeats], 
+            createdAt = it[FlightsTable.createdAt] 
+        )
+    }
+
+}
